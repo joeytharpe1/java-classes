6e49c57e9cc99623c87215a1afe4d100824ca7c6
Joseph Tharpe <solowun6@aol.com>
2022-03-14 17:57:30 -0700

    first commit

---
+++ src/com/codewithjoey/TextBox.java
@@ -0,0 +1,12 @@
+package com.codewithjoey;
+
+public class TextBox {
+    public String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void clear(){
+        text = "";
+    }
+}
