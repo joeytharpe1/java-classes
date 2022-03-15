e6cb1834cfcb46159e04dd800b0e53a4d3831f1c
Joseph Tharpe <solowun6@aol.com>
2022-03-14 18:04:57 -0700

    instansiating object from textbox class

--- src/com/codewithjoey/Main.java
+++ src/com/codewithjoey/Main.java
@@ -3,6 +3,12 @@ package com.codewithjoey;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TextBox textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
+
+        TextBox textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
     }
 }
--- src/com/codewithjoey/TextBox.java
+++ src/com/codewithjoey/TextBox.java
@@ -1,7 +1,7 @@
 package com.codewithjoey;
 
 public class TextBox {
-    public String text;
+    public String text = "";
 
     public void setText(String text) {
         this.text = text;
